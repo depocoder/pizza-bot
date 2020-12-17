6776b1764efc86e9b27373ab45b88c821fc27c58
herypank <60302361+herypank@users.noreply.github.com>
2020-12-17 22:41:24 +0300

    Добавил чтение json файла

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import json
 
 from loguru import logger
 
@@ -6,6 +7,9 @@ from loguru import logger
 @logger.catch
 def main():
     load_dotenv()
+    with open("addresses.json", "r") as my_file:
+        addresses = json.load(my_file)
+    print(addresses)
 
 
 if __name__ == "__main__":
