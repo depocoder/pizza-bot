2d34c08bf0af7a2fa8add168cc84d9b832ac5795
herypank <60302361+herypank@users.noreply.github.com>
2020-12-25 11:40:24 +0300

    Добавил отображение фото пицц

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from loguru import logger
 import requests
 from flask import Flask, request
 
-from motlin_api import get_access_token, get_products
+from motlin_api import get_access_token, get_products, get_image_link
 
 app = Flask(__name__)
 
@@ -50,18 +50,26 @@ def send_keyboard(recipient_id):
     params = {"access_token": os.getenv("PAGE_ACCESS_TOKEN")}
     headers = {"Content-Type": "application/json"}
     access_token = get_access_token(redis_conn)
-    keyboard_elements = [
-        {   
-            'title': product['name'] + ' ' + product['meta']['display_price']['with_tax']['formatted'],
-            'subtitle': product['description'],
-            'buttons': [
-                {
-                    'type': 'postback',
-                    'title': 'Положить в корзину',
-                    'payload': product['id'],
-                }
-            ]
-        } for product in get_products(access_token)[:5]]
+    keyboard_elements = []
+    for product in get_products(access_token)[:5]:
+        image_id = product['relationships']['main_image']['data']['id']
+        access_token = get_access_token(redis_conn)
+        image_link = get_image_link(access_token, image_id)['data']['link']['href']
+        keyboard_elements.append(
+            { 
+                'title': product['name'] + ' ' + product['meta']['display_price']['with_tax']['formatted'],
+                'subtitle': product['description'],
+                'image_url': image_link,
+                'buttons': [
+                    {
+                        'type': 'postback',
+                        'title': 'Положить в корзину',
+                        'payload': product['id'],
+                    }
+                ]
+            }
+        )
+
     pprint(keyboard_elements)
     data = json.dumps({
         'recipient': {
