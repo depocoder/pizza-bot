c025a46932f6dfdcd92890eb0537e0ad1a76f672
herypank <60302361+herypank@users.noreply.github.com>
2020-12-25 14:34:25 +0300

    Добавил отправку по категориям

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ from loguru import logger
 import requests
 from flask import Flask, request
 
-from motlin_api import get_access_token, get_products, get_image_link
+from motlin_api import (
+    get_access_token, get_products, get_image_link,
+    get_products_by_category_id)
 
 app = Flask(__name__)
 
@@ -46,17 +48,37 @@ def webhook():
     return "ok", 200
 
 
-def send_keyboard(recipient_id):
-    params = {"access_token": os.getenv("PAGE_ACCESS_TOKEN")}
-    headers = {"Content-Type": "application/json"}
+def get_keyboard_products(category_id):
+    keyboard_elements = [{ 
+                'title': 'Меню',
+                'subtitle': "Здесь вы можете выбрать один из вариантов",
+                'image_url': 'https://image.similarpng.com/very-thumbnail/2020/05/Pizza-logo-design-template-Vector-PNG.png',
+                'buttons': [
+                    {
+                        'type': 'postback',
+                        'title': 'Корзина',
+                        'payload': 'some text',
+                    },
+                    {
+                        'type': 'postback',
+                        'title': 'Акции',
+                        'payload': 'some text',
+                    },
+                    {
+                        'type': 'postback',
+                        'title': 'Сделать заказ',
+                        'payload': 'some text',
+                    }
+                ]
+            }]
     access_token = get_access_token(redis_conn)
-    keyboard_elements = []
-    for product in get_products(access_token)[:5]:
+    products = get_products_by_category_id(access_token, category_id)['data']
+    for product in products:
         image_id = product['relationships']['main_image']['data']['id']
         access_token = get_access_token(redis_conn)
         image_link = get_image_link(access_token, image_id)['data']['link']['href']
         keyboard_elements.append(
-            { 
+            {
                 'title': product['name'] + ' ' + product['meta']['display_price']['with_tax']['formatted'],
                 'subtitle': product['description'],
                 'image_url': image_link,
@@ -69,8 +91,14 @@ def send_keyboard(recipient_id):
                 ]
             }
         )
+    return keyboard_elements
 
-    pprint(keyboard_elements)
+
+def send_keyboard(recipient_id):
+    params = {"access_token": os.getenv("PAGE_ACCESS_TOKEN")}
+    headers = {"Content-Type": "application/json"}
+
+    keyboard_elements = get_keyboard_products("68ff879e-9b22-4cab-ab32-23cac76a40d9")
     data = json.dumps({
         'recipient': {
             "id": recipient_id
@@ -85,7 +113,6 @@ def send_keyboard(recipient_id):
             }
         }
     })
-    pprint(data)
     response = requests.post(
         'https://graph.facebook.com/v2.6/me/messages',
         headers=headers,
